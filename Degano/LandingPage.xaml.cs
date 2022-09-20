c79482a9cd80192e613c760279597cd0b045ff1a
TomasKvaraciejus <tom.kvaraciejus@gmail.com>
2022-09-20 11:10:56 +0300

    *fixed formatting

--- Degano/LandingPage.xaml.cs
+++ Degano/LandingPage.xaml.cs
@@ -5,20 +5,20 @@ public partial class LandingPage : ContentPage
 	public LandingPage()
 	{
 		InitializeComponent();
-    }
+	}
 
-    private void OnSignInClick(object sender, EventArgs e)
-    {
-        Navigation.PushAsync(new SignInPage());
-    }
+	private void OnSignInClick(object sender, EventArgs e)
+	{
+		Navigation.PushAsync(new SignInPage());
+	}
 
-    private void OnSignUpClick(object sender, EventArgs e)
-    {
-        Navigation.PushAsync(new SignUpPage());
-    }
+	private void OnSignUpClick(object sender, EventArgs e)
+	{
+		Navigation.PushAsync(new SignUpPage());
+	}
 
-    private void OnContinueWithoutSignInClick(object sender, EventArgs e)
-    {
-        Navigation.PushAsync(new MainPage());
-    }
+	private void OnContinueWithoutSignInClick(object sender, EventArgs e)
+	{
+		Navigation.PushAsync(new MainPage());
+	}
 }
