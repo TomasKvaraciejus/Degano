6ad50e96b15f00136f135ac5bb1e8e3e24fc05a8
Lukas <luk.brasiunas@gmail.com>
2022-10-04 00:39:56 +0300

    Getting more data using webscraper
    Added the ability to get location (coordinates) of the gas station using Google Maps API and also only the brand name (e.g.  Circle K, Neste, EMSI, ...)

--- Degano/webscraper.py
+++ Degano/webscraper.py
@@ -8,7 +8,10 @@ def get_fuel_data():
         'https://gas.didnt.work/?country=lt&brand=&city=Vilnius').text
     soup = BeautifulSoup(html_text, 'lxml')
 
-    data_file = open("Degano\data.txt", "w", encoding="utf-8")
+    data_file = open("data.txt", "w", encoding="utf-8")
+
+    gas_station_brands = ["Alauša", "Baltic Petroleum", "Circle K", "EMSI",
+                          "Ecoil", "Jozita", "Kvistija", "Neste", "Orlen", "Skulas", "Stateta", "Takuras", "Viada"]
 
     tbody = soup.find('tbody')
     trs = tbody.findAll('tr')
@@ -17,16 +20,34 @@ def get_fuel_data():
         tr.find('br').decompose()
         address = tr.find('small').text
         tr.find('small').decompose()
+
+        # Getting the coordinates of the given address using Google Maps API
+        address_for_url = address
+        address_for_url.replace(' ', '+')
+        url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + \
+            address_for_url + "&key=AIzaSyBbkz9JBShE8JYmYFoU2XG-jqIigrR4jyg"
+        response = requests.get(url)
+        json = response.json()
+        latitude = json['results'][0]['geometry']['location']['lat']
+        longitude = json['results'][0]['geometry']['location']['lng']
+
         gas_station_name = tr.find('td').text[2:]
+
+        # Getting only the gas station brand from the name
+        for brand in gas_station_brands:
+            if brand in gas_station_name:
+                gas_station_brand = brand
+                break
+
         diesel = tr.find('td').findNext('td').text
         petrol95 = tr.find('td').findNext('td').findNext('td').text
         petrol98 = tr.find('td').findNext(
             'td').findNext('td').findNext('td').text
         LPG = tr.find('td').findNext('td').findNext(
             'td').findNext('td').findNext('td').text
-        if diesel and petrol95 != "-":
+        if diesel and petrol95 != "-" and gas_station_name.strip() != "A. Lingės degalinė":
             data_file.write(
-                f"{gas_station_name.strip()}\n{address}\n{diesel}\n{petrol95}\n{petrol98}\n{LPG}\n")
+                f"{gas_station_brand}\n{gas_station_name.strip()}\n{address}\n{latitude}\n{longitude}\n{diesel}\n{petrol95}\n{petrol98}\n{LPG}\n")
 
     data_file.close()
 
