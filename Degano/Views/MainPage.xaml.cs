87a61b22178a6db0bdea92177d7b2d1fe7b00dea
TomasKvaraciejus <tom.kvaraciejus@gmail.com>
2022-10-11 23:30:18 +0300

    Implemented support for custom map markers

--- Degano/Views/MainPage.xaml.cs
+++ Degano/Views/MainPage.xaml.cs
@@ -125,7 +125,6 @@ namespace Degano.Views
 
 		private async void OnINeedGasClick(object sender, EventArgs e)
 		{
-            UserLocation.location.OpenInExternalApp();
 			await DisplayAlert("STOP", "You need gas", "OK");
 		}
 	}
