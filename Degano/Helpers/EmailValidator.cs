c9a422e95ba0196228402d1c15c8e257c7300302
Simukas1 <simasjarukaitis@gmail.com>
2022-10-18 12:11:38 +0000

    Fixing validations (#58)

---
+++ Degano/Helpers/EmailValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Degano.Helpers
+{
+    internal class EmailValidator
+    {
+        internal bool isValid { get; set; } = false;
+        private Regex regex { get; } = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        internal bool IsEmailValid(string email)
+        {
+            Match match = regex.Match(email);
+            isValid = match.Success;
+            return match.Success;
+        }
+    }
+}
---
+++ Degano/Helpers/PasswordValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Degano.Helpers
+{
+    internal class PasswordValidator
+    {
+        internal bool isValid { get; set; } = false;
+        internal string ErrorMessage { get; set; }
+        internal bool IsPasswordValid(string text)
+        {
+            if (text.Length < 10)
+            {
+                ErrorMessage = "Password should contain at least 10 characters!";
+                isValid = false;
+                return false;
+            }
+            if (Regex.IsMatch(text, @"\s"))
+            {
+                ErrorMessage = "Password should not contain whitespaces!";
+                isValid = false;
+                return false;
+            }
+            if (!text.Any(char.IsDigit))
+            {
+                ErrorMessage = "Password should contain at least one digit!";
+                isValid = false;
+                return false;
+            }
+            isValid = true;
+            return true;
+        }
+    }
+}
