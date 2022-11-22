78e18c9e029213017e27744577f470ca6739ac81
Lukas <luk.brasiunas@gmail.com>
2022-11-22 15:05:19 +0200

    Firebase exception fix

--- Degano/ExceptionLogger.cs
+++ Degano/ExceptionLogger.cs
@@ -36,7 +36,7 @@ namespace Degano
                 BasePath = "https://degano-70426-default-rtdb.europe-west1.firebasedatabase.app/"
             };
             IFirebaseClient client = new FirebaseClient(config);
-            FirebaseResponse response = await client.SetAsync("Exceptions/", ex.ToJson);
+            FirebaseResponse response = await client.SetAsync("Exceptions/" + DateTime.Now.ToString("MM-dd-yyyy_HH-mm-ss"), ex.exMessage);
         }
     }
 }
